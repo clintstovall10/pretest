f8c2ee23e7e96f4a82fb6157bcb8f1fa13435ce6
Clinton Stovall <cstova01@uafs.edu>
2026-08-21 09:46:22 -0500

    friday work

--- pretest/prob1/Prob1.java
+++ pretest/prob1/Prob1.java
@@ -3,7 +3,37 @@ package prob1;
 public class Prob1 {
 
 	public static void main(String[] args) {
-		System.out.printf("Hello world!");
+		
+		int arr1 [] = {4,7,2,9,1};
+		
+		int length = arr1.length;
+		int prevNUM = 0;
+		
+		
+		for (int i = 0; i < length; i++) {
+			
+			int newNUM = arr1[i]-prevNUM;
+			
+			prevNUM = arr1[i];
+			
+			System.out.println(newNUM);
+			
+			
+			
+			
+		}
+		
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 
 	}
 
