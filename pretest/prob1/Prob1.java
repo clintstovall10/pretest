7c83546e4e9d55ba334645036773b45db50b8514
Clinton Stovall <cstova01@uafs.edu>
2026-08-22 12:47:33 -0500

    Prob1

--- pretest/prob1/Prob1.java
+++ pretest/prob1/Prob1.java
@@ -4,27 +4,41 @@ public class Prob1 {
 
 	public static void main(String[] args) {
 		
+		//During programming 1 last semester we did not cover methods a
+		//whole let and kind of rushed through them. I remember them slightly,
+		//just can't remember the syntax without looking it up of course.
+		
 		int arr1 [] = {4,7,2,9,1};
 		
 		int length = arr1.length;
-		int prevNUM = 0;
+		int prevNUM = arr1[0];
+		int maxNUM = arr1[0];
 		
-		
-		for (int i = 0; i < length; i++) {
+		for (int i = 1; i < length; i++) {
 			
 			int newNUM = arr1[i]-prevNUM;
 			
+			if (newNUM < 0) {
+				
+				newNUM = newNUM * -1;
+				
+				if (newNUM > maxNUM) {
+					
+					maxNUM = newNUM;
+				}
+				
+			}else {
+				if (newNUM > maxNUM) {
+					
+					maxNUM = newNUM;
+				}
+			}
+			
 			prevNUM = arr1[i];
-			
-			System.out.println(newNUM);
-			
-			
-			
 			
 		}
 		
-	
-		
+		System.out.println(maxNUM);
 		
 		
 		
